9b67e83f399e143ced55c64ba2c73e76df66d166
Fabrizzio Rivera <frivera@nevicad.com>
2019-04-24 21:43:43 -0600

    server connection

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
+import { BackendService } from '../backend.service'
 
 @Component({
   selector: 'app-home',
@@ -13,13 +14,34 @@ export class HomePage {
   title: string = ''
   repetition: string = 'una vez'
 
-  constructor(public alertController: AlertController){}
+  constructor(
+    public alertController: AlertController, 
+    private backend: BackendService,
+    private toastController: ToastController,
+    ){}
 
   alarms: Array<{title: string, time: any, repetition: string, on: boolean}> = []
 
-  createAlarm(){    
-    this.alarms.push({'title': this.title, 'time': this.time, 'repetition': this.repetition, 'on': true})
-    this.create = false
+  async createAlarm(){
+    const alarm = {'title': this.title, 'time': this.time, 'repetition': this.repetition, 'on': true}
+    const response = await this.backend.postAlarm(alarm);
+
+    if (response.status){
+      this.alarms.push(alarm)
+      this.create = false
+
+      this.presentToast('Alarma agregada')
+    } else {
+      this.presentToast('No se pudo agregar alarma')
+    }
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
   }
 
   alarmPrompt(){
